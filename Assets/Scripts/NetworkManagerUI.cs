d055ef0c2e0942c35b64c824b875552e7971adce
Ncoldd <n.colderwood1@andersonuniversity.edu>
2026-04-13 14:12:02 -0400

    Add NetworkManager and host/client UI

---
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.UI; 
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
+    private void Awake()
+    {
+        hostBtn.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
+        clientBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+    }
+}
